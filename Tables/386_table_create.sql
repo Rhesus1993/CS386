eed5dd2bffa34f170e4c4fe1a02b04050bff7b4b
Riley Shelton <rs854@nau.edu>
2015-04-02 14:56:22 -0700

    Combined Table SQL
    Needs more work I just wanted to get it up online

---
+++ Tables/386_table_create.sql
@@ -0,0 +1,36 @@
+Create Table SoftwareE_User(
+	User_ID number(5) Constraint User_PK Primary Key DEFERRABLE INITIALLY IMMEDIATE,
+	User_FName varchar2(10) Constraint User_FName_NN Not Null DEFERRABLE INITIALLY IMMEDIATE,
+	User_LName varchar2(10) Constraint User_LName_NN Not Null DEFERRABLE INITIALLY IMMEDIATE,
+	User_Status varchar2(10) Constraint User_Status_NN Not Null DEFERRABLE INITIALLY IMMEDIATE,
+	Position_Code number(4) Constraint UPosition_Code_NN Not Null DEFERRABLE INITIALLY IMMEDIATE,
+);
+
+Create Table SoftwareE_Position(
+	Position_Code number(4) Constraint Position_PK Primary Key DEFERRABLE INITIALLY IMMEDIATE,
+	Position_Title varchar2(10) Constraint User_FName_NN Not Null DEFERRABLE INITIALLY IMMEDIATE,
+	Privilege int Constraint User_FName_NN Not Null DEFERRABLE INITIALLY IMMEDIATE,
+);
+
+Create Table SoftwareE_Assigned_Review(
+
+);
+
+Create Table SoftwareE_Review(
+	Review_ID number(4) auto_increment Constraint Review_ID_PK Primary Key DEFERRABLE INITIALLY IMMEDIATE,
+	Review_Flag varchar2(10) Constraint User_FName_NN Not Null DEFERRABLE INITIALLY IMMEDIATE,
+	Leadership_Ability int Constraint User_FName_NN Not Null DEFERRABLE INITIALLY IMMEDIATE, 
+	Follow_Directions int Constraint User_FName_NN Not Null DEFERRABLE INITIALLY IMMEDIATE,
+	Gen_Contributions varchar2(250) Constraint User_FName_NN Not Null DEFERRABLE INITIALLY IMMEDIATE,
+	Technical_ability int Constraint User_FName_NN Not Null DEFERRABLE INITIALLY IMMEDIATE,
+	Creativity int Constraint User_FName_NN Not Null DEFERRABLE INITIALLY IMMEDIATE,
+	Punctionality int Constraint User_FName_NN Not Null DEFERRABLE INITIALLY IMMEDIATE,
+	Availability int Constraint User_FName_NN Not Null DEFERRABLE INITIALLY IMMEDIATE,
+	Attentiveness int Constraint User_FName_NN Not Null DEFERRABLE INITIALLY IMMEDIATE,
+	Comments varchar2(250),
+	Work_in_Groups int Constraint User_FName_NN Not Null DEFERRABLE INITIALLY IMMEDIATE,
+	About_User_ID number(5) Constraint User_FName_NN Not Null DEFERRABLE INITIALLY IMMEDIATE,
+	From_User_ID number(5) Constraint User_FName_NN Not Null DEFERRABLE INITIALLY IMMEDIATE,
+);
+
+Alter Table SoftwareE_User Add Constraint Position_FK Foreign Key(Position_Code) References SoftwareE_Position(Position_Code) DEFERRABLE INITIALLY IMMEDIATE;
