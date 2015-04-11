4cf4819fab441bfbaba29c0b8d4a90cce0835664
Riley Shelton <rs854@nau.edu>
2015-04-11 12:25:51 -0700

    Insert table

---
+++ Tables/add_reviews.sql
@@ -0,0 +1,23 @@
+Insert Into Review(Review_Flag, Leadership_Ability, Follow_Directions, Gen_Contributions, Technical_ability, Creativity, Punctionality, Availability, Attentiveness, Comments, Work_in_Groups, About_User_ID, From_User_ID)
+Values
+('Completed', 1, 1, 'None', 4, 2, 1, 3, 1, 'None', 2, 69, 90000);
+
+Insert Into Review(Review_Flag, Leadership_Ability, Follow_Directions, Gen_Contributions, Technical_ability, Creativity, Punctionality, Availability, Attentiveness, Comments, Work_in_Groups, About_User_ID, From_User_ID)
+Values
+('Completed', 1, 3, 'None', 4, 3, 5, 3, 1, 'None', 4, 12, 90000);
+
+Insert Into Review(Review_Flag, Leadership_Ability, Follow_Directions, Gen_Contributions, Technical_ability, Creativity, Punctionality, Availability, Attentiveness, Comments, Work_in_Groups, About_User_ID, From_User_ID)
+Values
+('Completed', 3, 3, 'Everything', 4, 3, 5, 3, 4, 'Everything', 4, 12, 90000
+
+Insert Into Review(Review_ID, Review_Flag, Leadership_Ability, Follow_Directions, Gen_Contributions, Technical_ability, Creativity, Punctionality, Availability, Attentiveness, Comments, Work_in_Groups, About_User_ID, From_User_ID)
+Values
+(4,'TODO', 0, 0, 'N/A', 0, 0, 0, 0, 0, 'N/A', 0, 90000, 69);
+
+Insert Into Review(Review_ID,Review_Flag, Leadership_Ability, Follow_Directions, Gen_Contributions, Technical_ability, Creativity, Punctionality, Availability, Attentiveness, Comments, Work_in_Groups, About_User_ID, From_User_ID)
+Values
+(5, 'TODO', 0, 0, 'N/A', 0, 0, 0, 0, 0, 'N/A', 0, 90000, 12);
+
+Insert Into Review(Review_ID,Review_Flag, Leadership_Ability, Follow_Directions, Gen_Contributions, Technical_ability, Creativity, Punctionality, Availability, Attentiveness, Comments, Work_in_Groups, About_User_ID, From_User_ID)
+Values
+(6, 'TODO', 0, 0, 'N/A', 0, 0, 0, 0, 0, 'N/A', 0, 90000, 42);
--- Tables/insert_user.sql
+++ Tables/insert_user.sql
@@ -1,1 +1,6 @@
-Insert into SoftwareE_User(User_ID, User_FName, User_LName, User_Status, Position_Code, User_Login, User_Password) Values (00001, 'Riley', 'Shelton', '0', 0001, 'Potato Cat');
+Insert into SoftwareE_User(User_ID, User_FName, User_LName, User_Status, Position_Code, User_Login, User_Password) 
+Values (00001, 'Riley', 'Shelton', '0', 0001, 0, 'Potato Cat');
+Insert into SoftwareE_User(User_FName, User_LName, User_Status, Position_Code, User_Login, User_Password) 
+Values ('Makayala', 'Shepherd', '0', 0001, 0, 'Cats Cats');
+Insert into SoftwareE_User(User_FName, User_LName, User_Status, Position_Code, User_Login, User_Password) 
+Values ('John', 'Loudon', '0', 0001, 0, 'More Cats');
